2d7ffc4a350c5596d875133870adb25695b46df1
Iryna Aleksandrova <ira_aleksandrova@ukr.net>
2025-02-06 20:41:57 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+// const App = () => {
+//   return (
+//     <div className="container">
+//       <Profile
+//         name={userData.username}
+//         tag={userData.tag}
+//         location={userData.location}
+//         image={userData.avatar}
+//         stats={userData.stats}
+//       />
+//       <FriendList friends={friends} />
+//       <TransactionHistory items={transactions} />
+//     </div>
+//   );
+// };
+
+export default App;
