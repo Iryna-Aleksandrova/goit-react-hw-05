fe0e741981caa44955de5d0978ca662799225610
Iryna-Aleksandrova <ira_aleksandrova@ukr.net>
2025-02-10 19:36:35 +0000

    hw 5 build

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,8 @@
-import css from "./NotFoundPage.module..css";
-import { TbError404 } from "react-icons/tb";
+import css from "../NotFoundPage/NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
     <div className={css.div}>
-      <TbError404 style={{ fontSize: "24px", color: "#da7b93" }} />
       <p>Not found 404</p>
     </div>
   );
